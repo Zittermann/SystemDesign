14b4850821921d31b38411c690ef09427c95a84e
Zittermann <germanzittermann@gmail.com>
2021-10-26 19:22:32 -0300

    Agregamos patron Singleton

---
+++ Singleton/src/domain/Singleton.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Singleton {
+
+    public static Singleton instance;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getInstance() {
+
+        if(instance == null){
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+
+}
