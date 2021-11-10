4640d010caee61f18c06fa26312bed886b41873d
Zittermann <germanzittermann@gmail.com>
2021-11-09 21:24:14 -0300

    Mejoramos Factory

--- Factory/src/test/SoldadoTest.java
+++ Factory/src/test/SoldadoTest.java
@@ -3,23 +3,14 @@ package test;
 import domain.Soldado;
 import domain.SoldadoFactory;
 
-import java.util.Scanner;
-
 public class SoldadoTest {
     public static void main(String[] args) {
 
         SoldadoFactory soldadoFactory = new SoldadoFactory();
-        Soldado soldado = null;
+        Soldado mosquetero = soldadoFactory.crearMosquetero();
 
-
-        Scanner s = new Scanner(System.in);
-
-        System.out.println("(G)uerrero o (M)osquetero?");
-
-        soldado = soldadoFactory.crearSoldado(s.nextLine());
-
-        soldado.caminar();
-        soldado.atacar();
+        mosquetero.caminar();
+        mosquetero.atacar();
 
     }
 
